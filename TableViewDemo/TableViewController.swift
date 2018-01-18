348be93a9b79509a3c2e0ee0713f6d578cdf990b
Peter Reichl <pjreichl@gmail.com>
2018-01-18 13:57:57 -0500

    Add Multiple Sections

--- TableViewDemo/TableViewController.swift
+++ TableViewDemo/TableViewController.swift
@@ -10,18 +10,33 @@ import UIKit
 
 class TableViewController: UIViewController {
     
+    var items = [DataItem]()
+    var otherItems = [DataItem]()
+    var allItems = [DataItem]()
+    
     @IBOutlet weak var tableView: UITableView!
     
-    extension TableViewController: UITableViewDataSource {
-        
-    }
-    
-    
-    extension TableViewController: UITableViewDelegate {
-        
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 1...12 {
+            if i > 9 {
+                items.append(DataItem(title: "Title #\(i)", subtitle: "This is subtitle #\(i)", imageName: "img\(i).jpg"))
+            } else {
+                items.append(DataItem(title: "Title #0\(i)", subtitle: "This is subtitle #0\(i)", imageName: "img0\(i).jpg"))
+            }
+        }
+        
+        for i in 1...12 {
+            if i > 9 {
+                otherItems.append(DataItem(title: "Another Title #\(i)", subtitle: "This is another subtitle #\(i)", imageName: "anim\(i).jpg"))
+            } else {
+                otherItems.append(DataItem(title: "Another Title #0\(i)", subtitle: "This is another subtitle #0\(i)", imageName: "anim0\(i).jpg"))
+            }
+        }
+        
+        allItems.append(items)
+        allItems.append(otherItems)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -29,7 +44,35 @@ class TableViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allItems[section].count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return allItems.count
+    }
 }
 
+extension TableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let item = allItems[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.subtitle
+        
+        if let imageView = cell.imageView, let itemImage = item.image {
+            imageView.image = itemImage
+        } else {
+            cell.imageView?.image = nil
+        }
+        
+        return cell
+    }
+}
+
+
+extension TableViewController: UITableViewDelegate {
+    
+}

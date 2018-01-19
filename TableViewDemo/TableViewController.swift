f8c81fdf2c2ed9c0880c0d53f32cebaf1a699dd3
Peter Reichl <pjreichl@gmail.com>
2018-01-19 17:19:32 -0500

    Completed code for lesson

--- TableViewDemo/TableViewController.swift
+++ TableViewDemo/TableViewController.swift
@@ -12,12 +12,42 @@ class TableViewController: UIViewController {
     
     var items = [DataItem]()
     var otherItems = [DataItem]()
-    var allItems = [DataItem]()
+    var allItems = [[DataItem]]()
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.allowsSelectionDuringEditing = true
+        navigationItem.rightBarButtonItem = editButtonItem
+        
+        func setEditing(_ editing: Bool, animated: Bool) {
+            super.setEditing(editing, animated: animated)
+            
+            if editing {
+                tableView.beginUpdates()
+                
+                for (index, sectionItems) in allItems.enumerated() {
+                    let indexPath = IndexPath(row: sectionItems.count, section: index)
+                    tableView.insertRows(at: [indexPath], with: .fade)
+                }
+                
+                tableView.endUpdates()
+                tableView.setEditing(true, animated: true)
+            } else {
+                tableView.beginUpdates()
+                
+                for (index, sectionItems) in allItems.enumerated() {
+                    let indexPath = IndexPath(row: sectionItems.count, section: index)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+                
+                tableView.endUpdates()
+                tableView.setEditing(false, animated: true)
+            }
+        }
+        
         
         for i in 1...12 {
             if i > 9 {
@@ -46,7 +76,8 @@ class TableViewController: UIViewController {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allItems[section].count
+        let addedRow = isEditing ? 1 : 0
+        return allItems[section].count + addedRow
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -57,6 +88,11 @@ class TableViewController: UIViewController {
 extension TableViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        if indexPath.row >= allItems[indexPath.section].count && isEditing {
+            cell.textLabel?.text = "Add New Item"
+            cell.detailTextLabel?.text = nil
+            cell.imageView?.image = nil
+        } else {
         let item = allItems[indexPath.section][indexPath.row]
         
         cell.textLabel?.text = item.title
@@ -67,12 +103,70 @@ extension TableViewController: UITableViewDataSource {
         } else {
             cell.imageView?.image = nil
         }
-        
+        }
         return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section #\(section)"
+}
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            allItems[indexPath.section].remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            let newData = DataItem(title: "New Data", subtitle: "", imageName: nil)
+            allItems[indexPath.section].append(newData)
+            tableView.insertRows(at: [indexPath], with: .fade)
+        }
+    }
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count && isEditing {
+            return false
+        }
+        return true
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = allItems[sourceIndexPath.section][sourceIndexPath.row]
+        
+        allItems[sourceIndexPath.section].remove(at: sourceIndexPath.row)
+        
+        if sourceIndexPath.section == destinationIndexPath.section {
+            allItems[sourceIndexPath.section].insert(itemToMove, at: destinationIndexPath.row)
+        } else {
+            allItems[destinationIndexPath.section].insert(itemToMove, at: destinationIndexPath.row)
+        }
+    }
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        let sectionItems = allItems[proposedDestinationIndexPath.section]
+        if proposedDestinationIndexPath.row >= sectionItems.count {
+            return IndexPath(row: sectionItems.count - 1, section: proposedDestinationIndexPath.section)
+        }
+        return proposedDestinationIndexPath
     }
 }
 
-
 extension TableViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count {
+            return .insert
+        } else {
+            return .delete
+        }
+    }
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        let sectionItems = allItems[indexPath.section]
+        if isEditing && indexPath.row < sectionItems.count {
+            return nil
+        }
+        return indexPath
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count && isEditing {
+            self.tableView(tableView, commit: .insert, forRowAt: indexPath)
+        }
+    }
 }
